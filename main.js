71c4f87c807167af72cf01fc947b32777551c1d1
Steven Schwengel <75495171+MustardTop@users.noreply.github.com>
2021-02-03 01:25:54 -0500

    pulling initial content from git api

--- main.js
+++ main.js
@@ -8,53 +8,30 @@ console.log("hello");
 
 const BASE_URL = 'https://api.github.com/users/StevenSchwengel';
 
+   // const generateHTML2 = (data) => {
+   //   console.log(data);
+   //   console.log(data.name);
+   //   console.log(data.organizations_url);
+   //   var repo_url = data.repos_url;
+   //   console.log ("repo_url", repo_url);
+   //   console.log(data.repos_url);
+   //   console.log(data.avatar_url);
+   //   console.log(data.public_repos);
+   //   // console.log(data);
+   // }
 
-
-//    const generateHTML = (data) => {
-//      console.log('data', data);
-//      const source = document.getElementById("rover-image-template").innerHTML;
-//      const template = Handlebars.compile(source);
-//      const context = data;
-//      const html = template(context);
-//
-//      document.querySelector('.container').innerHTML = html;
-// }
-
-    //
-    // const source = document.getElementbyID("rover-image-template").innerHTML;
-    // const template = Handlebars
-
-   //
-   // fetch(`${BASE_URL}/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${earthDate}&api_key=${API_KEY}`)
-   //  .then(response => response.json())
-   //  .then(data => generateHTML(data));
 
  const generateHTML = (data) => {
    console.log(data);
-   console.log(data.name);
-   console.log(data.organizations_url);
+   const name = data.name;
+   const org_url = data.organizations_url;
    console.log(data.repos_url);
-   console.log(data.avatar_url);
-   console.log(data.public_repos);
-   // console.log(data);
- }
-
-
-  // const generateHTML2 = (data) => {
-  //   console.log(data);
-  // }
-
-
-
- fetch(`${BASE_URL}/repos`,) {
-     headers: {
-       Authorization: `token ${GITHUB_TOKEN}`
-     }
-   })
-     .then(response => response.json())
-     // .then(data => console.log(data));
-     .then(data => generateHTML2(data));
- })();
+   const avatar_url = data.avatar_url;
+   console.log(avatar_url);
+   fetch(avatar_url);
+   const public_data = data.public_repos;
+   console.log(org_url);
+  }
 
 
 
@@ -68,6 +45,17 @@ fetch(`${BASE_URL}`, {
     .then(data => generateHTML(data));
 })();
 
+
+// fetch("https://api.github.com/users/StevenSchwengel/repos", {
+// fetch(`${BASE_URL}`, {
+//     headers: {
+//       Authorization: `token ${GITHUB_TOKEN}`
+//     }
+//   })
+//     .then(response => response.json())
+//     // .then(data => console.log(data));
+//     .then(data => generateHTML2(data));
+// })();
 
 // const fullName = data.name;
 // console.log(fullName);
